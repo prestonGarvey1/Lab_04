5b4341deae0ac8178ff30d615044e2a2d2bec3d2
Minorridge <ridgetolliver3@gmail.com>
2025-09-08 08:22:00 -0400

    Add values

---
+++ src/Task1.java
@@ -0,0 +1,27 @@
+
+public class Task1 {
+    public static void main(String[] args) {
+        int intOperandA;
+        int intOperandB;
+        int intSum;
+        int intDifference;
+        int intProduct;
+        int intQuotient;
+        int intModulo;
+        double doubleOperandA;
+        double doubleOperandB;
+        double doubleSum;
+        double doubleProduct;
+        double doubleDifference;
+        double doubleQuotient;
+        double doubleModulo;
+
+
+
+
+
+
+
+
+    }
+}
